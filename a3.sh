69e97e1dece2cc7767ab7a84d039c4aea95b70d1
ssgrandhi <vvsgupta008@gmail.com>
2026-04-26 18:14:32 +0530

    Update a3.sh

--- a3.sh
+++ a3.sh
@@ -15,7 +15,7 @@ fi
 sum=$(echo "$num1 + $num2" | bc)
 diff=$(echo "$num1 - $num2" | bc)
 prod=$(echo "$num1 * $num2" | bc)
-added file 12334
+added file 123345
 # Handle division by zero
 if (( $(echo "$num2 == 0" | bc -l) )); then
     quotient="undefined (division by zero)"
